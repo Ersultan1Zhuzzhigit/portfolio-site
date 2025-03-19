4b140ae288876fd2f12b9c0ae6f4bfbf99329958
Ersultan1Zhuzzhigit <ersultanubt@gmail.com>
2025-03-19 22:53:19 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contact-btn").addEventListener("click", function() {
+    window.location.href = "https://wa.me/77058084825?text=Сәлем!%20Веб-сайт%20жасау%20бойынша%20келіп%20тұрмын.";
+});
